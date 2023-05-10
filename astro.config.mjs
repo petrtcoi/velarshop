b2b5a2478f12a3595049d2e0442eebeaf56791d2
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-05-10 20:07:39 +0300

    add compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import image from "@astrojs/image"
-import mdx from "@astrojs/mdx"
-import preact from "@astrojs/preact"
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: 'https://velarshop.ru',
-  integrations: [
-    tailwind(),
-    image(),
-    mdx(),
-    preact(),
-    sitemap(),
-    robotsTxt( { sitemap: [ "https://velarshop.ru/sitemap-index.xml" ] } )
-  ]
-} )
+  integrations: [tailwind(), image(), mdx(), preact(), sitemap(), robotsTxt({
+    sitemap: ["https://velarshop.ru/sitemap-index.xml"]
+  }), compress()]
+});
