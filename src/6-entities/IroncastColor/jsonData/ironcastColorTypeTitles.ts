d135f1aa271a19da8de93377ee848c6ad9ed9fa1
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-10-26 12:01:36 +0300

    fix iron casts

--- src/6-entities/IroncastColor/jsonData/ironcastColorTypeTitles.ts
+++ src/6-entities/IroncastColor/jsonData/ironcastColorTypeTitles.ts
@@ -1,24 +1,25 @@
-import type { IroncastColorTypeTitles } from "../types/IroncastColor"
+import type { IroncastColorTypeTitles } from "../types/IroncastColor.type";
 
 export const ironcastColorTypeTitles: IroncastColorTypeTitles[] = [
   {
-    id: 'design_ral_any',
-    title: 'Цвет из палитры RAL'
+    id: "design_ral_any",
+    title: "Цвет из палитры RAL",
   },
   {
-    id: 'color_emal',
-    title: 'Термостойкая эмаль'
+    id: "color_emal",
+    title: "Термостойкая эмаль",
   },
   {
-    id: 'patination',
-    title: 'Патина'
+    id: "patination",
+    title: "Патина",
   },
   {
-    id: 'two_colors',
-    title: 'Комбинированная (в два цвета - с набивкой узоров) + лак'
+    id: "two_colors",
+    title: "Комбинированная (в два цвета - с набивкой узоров) + лак",
   },
   {
-    id: 'design',
-    title: 'Дизайнерское исполнение (нанесение специальных структур по образцу заказчика)'
+    id: "design",
+    title:
+      "Дизайнерское исполнение (нанесение специальных структур по образцу заказчика)",
   },
-]
+];
