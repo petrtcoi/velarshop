90d34cd7e2077a95f4991e638be7068241ff3ee6
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-10-30 10:38:03 +0300

    add Offers

--- src/4-widgets/pageModel/ModelPrice/ModelPrice.tsx
+++ src/4-widgets/pageModel/ModelPrice/ModelPrice.tsx
@@ -1,31 +1,42 @@
-import ButtonAnchorLink from "@shared/components/ButtonAnchorLink"
-import { NO_PRICE } from "../constants/noPrice"
+import ButtonAnchorLink from "@shared/components/ButtonAnchorLink";
+import { NO_PRICE } from "../constants/noPrice";
 
 type Props = {
-  minPrice: string
-}
+  minPrice: string;
+};
 
-function ModelPrice ({ minPrice }: Props) {
-  if (minPrice === NO_PRICE) return null
+function ModelPrice({ minPrice }: Props) {
+  if (minPrice === NO_PRICE) return null;
 
   return (
     <div
-      id='buy-now'
-      class='py-5 flex flex-row gap-8 items-end justify-center'
+      id="buy-now"
+      class="py-5 flex flex-row gap-8 items-end justify-center"
+      itemProp="offers"
+      itemScope
+      itemType="https://schema.org/AggregateOffer"
     >
+      <meta
+        itemProp="priceCurrency"
+        content="RUB"
+      />
+      <meta
+        itemProp="lowPrice"
+        content={minPrice.toString()}
+      />
       <div>
-        <span class='text-sm'>цена от:</span>
-        <span class='mx-2 text-4xl text-neutral-900'>
-          { parseInt(minPrice).toLocaleString("ru-RU") }
+        <span class="text-sm">цена от:</span>
+        <span class="mx-2 text-4xl text-neutral-900">
+          {parseInt(minPrice).toLocaleString("ru-RU")}
         </span>
-        <span class='text-small'>&#8381;</span>
+        <span class="text-small">&#8381;</span>
       </div>
       <ButtonAnchorLink
-        title='Купить'
-        anchor='#radiators-list'
+        title="Купить"
+        anchor="#radiators-list"
       />
     </div>
-  )
+  );
 }
 
-export default ModelPrice
+export default ModelPrice;
