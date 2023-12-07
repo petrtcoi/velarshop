afdd386521759400173966572d9df335873f2cd2
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-12-08 02:47:36 +0300

    update prices

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import image from "@astrojs/image"
-import mdx from "@astrojs/mdx"
-import preact from "@astrojs/preact"
-import sitemap from '@astrojs/sitemap'
-import robotsTxt from "astro-robots-txt"
-
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import preact from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig( {
+export default defineConfig({
   site: 'https://velarshop.ru',
-  integrations: [ tailwind(), image(), mdx(), preact(), sitemap(), robotsTxt( {
-    sitemap: [ "https://velarshop.ru/sitemap-index.xml" ]
-  } ) ],
-} )
+  integrations: [
+    tailwind(),
+    image(),
+    mdx(),
+    preact(),
+    sitemap(),
+    robotsTxt({
+      sitemap: ['https://velarshop.ru/sitemap-index.xml'],
+    }),
+  ],
+});
