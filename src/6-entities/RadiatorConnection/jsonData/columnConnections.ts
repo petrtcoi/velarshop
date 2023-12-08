b818b865fe452378eb6c860578e977fc8ba4c690
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-12-08 03:10:22 +0300

    update prices

--- src/6-entities/RadiatorConnection/jsonData/columnConnections.ts
+++ src/6-entities/RadiatorConnection/jsonData/columnConnections.ts
@@ -1,40 +1,40 @@
-import type { ColumnRadiatorConnection } from "../types/RadiatorConnection.type"
+import type { ColumnRadiatorConnection } from '../types/RadiatorConnection.type';
 
 export const columnConnections: ColumnRadiatorConnection[] = [
   {
-    id: "lat1/2",
-    code: "БОК 1/2",
-    description: "Боковое подключение 1/2",
+    id: 'lat1/2',
+    code: 'БОК 1/2',
+    description: 'Боковое подключение 1/2',
     priceRub: 0,
   },
   {
-    id: "lat3/4",
-    code: "БОК 3/4",
-    description: "Боковое подключение 3/4",
+    id: 'lat3/4',
+    code: 'БОК 3/4',
+    description: 'Боковое подключение 3/4',
     priceRub: 0,
   },
   {
-    id: "b50",
-    code: "B50 1/2",
-    description: "Нижнее одностороннее 50 мм",
-    priceRub: 5000,
+    id: 'b50',
+    code: 'B50 1/2',
+    description: 'Нижнее одностороннее 50 мм',
+    priceRub: 5500,
   },
   {
-    id: "blr",
-    code: "BLR 1/2",
-    description: "Нижнее разностороннее",
-    priceRub: 5000,
+    id: 'blr',
+    code: 'BLR 1/2',
+    description: 'Нижнее разностороннее',
+    priceRub: 5500,
   },
   {
-    id: "v50",
-    code: "V50 1/2",
-    description: "Нижнее одностороннее 50 мм c термовентилем",
-    priceRub: 7500,
+    id: 'v50',
+    code: 'V50 1/2',
+    description: 'Нижнее одностороннее 50 мм c термовентилем',
+    priceRub: 8250,
   },
   {
-    id: "vlr",
-    code: "VLR 1/2",
-    description: "Нижнее разностороннее с термовентилем",
-    priceRub: 7500,
+    id: 'vlr',
+    code: 'VLR 1/2',
+    description: 'Нижнее разностороннее с термовентилем',
+    priceRub: 8250,
   },
-]
+];
