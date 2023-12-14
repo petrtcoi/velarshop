cd3252825924e0bc773036bc6ed36f62932682ae
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2023-12-14 19:15:51 +0300

    update column colors

--- src/6-entities/ColumnsColor/jsonData/columnColors.ts
+++ src/6-entities/ColumnsColor/jsonData/columnColors.ts
@@ -1,34 +1,34 @@
-import type { ColumnColor } from "../types/ColumnColor.type"
+import type { ColumnColor } from '../types/ColumnColor.type';
 
 export const columnColors: ColumnColor[] = [
   {
-    id: "ral_9016_gloss",
-    title: "Белый RAL 9016 глянец",
-    shortName: "RAL 9016 глянец",
+    id: 'ral_9016_gloss',
+    title: 'Белый RAL 9016 глянец',
+    shortName: 'RAL 9016 глянец',
     multiplicate: 1,
   },
   {
-    id: "ral_9016_matte",
-    title: "Белый RAL 9016 матовый",
-    shortName: "RAL 9016 мат.",
-    multiplicate: 1,
+    id: 'ral_9016_matte',
+    title: 'Белый RAL 9016 матовый',
+    shortName: 'RAL 9016 мат.',
+    multiplicate: 1.3,
   },
   {
-    id: "ral_9005_gloss",
-    title: "Черный RAL 9005 глянец",
-    shortName: "RAL 9005 глянец",
-    multiplicate: 1,
+    id: 'ral_9005_gloss',
+    title: 'Черный RAL 9005 глянец',
+    shortName: 'RAL 9005 глянец',
+    multiplicate: 1.3,
   },
   {
-    id: "ral_9005_matte",
-    title: "Черный RAL 9005 матовый",
-    shortName: "RAL 9005 мат.",
-    multiplicate: 1,
+    id: 'ral_9005_matte',
+    title: 'Черный RAL 9005 матовый',
+    shortName: 'RAL 9005 мат.',
+    multiplicate: 1.3,
   },
   {
-    id: "ral_any",
-    title: "По шкале RAL",
-    shortName: "свой RAL",
-    multiplicate: 1.1,
+    id: 'ral_any',
+    title: 'По шкале RAL',
+    shortName: 'свой RAL',
+    multiplicate: 1.3,
   },
-]
+];
