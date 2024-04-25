af6f15f7dbc7c3be55674f2d0e0b12b4933dc7f9
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-04-25 10:25:09 +0300

    feat: start news

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, z } from 'astro:content';
 
 const pageSchema = defineCollection({
   schema: z.object({
     title: z.string(),
     slug: z.string(),
-    description: z.string().optional()
-  })
-})
+    description: z.string().optional(),
+  }),
+});
 
 const articleSchema = defineCollection({
   schema: z.object({
@@ -15,24 +15,34 @@ const articleSchema = defineCollection({
     slug: z.string(),
     thumbnail: z.string(),
     sortRank: z.number(),
-  })
-})
+  }),
+});
 
+const newsSchema = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    short_description: z.string(),
+    slug: z.string(),
+    thumbnail: z.string(),
+    date: z.string(),
+  }),
+});
 
 const modelDescriptionSchema = defineCollection({
-  schema: z.object({})
-})
+  schema: z.object({}),
+});
 const itemDescriptionSchema = defineCollection({
-  schema: z.object({})
-})
+  schema: z.object({}),
+});
 const modelAdditionalInfoSchema = defineCollection({
-  schema: z.object({ title: z.string().optional() })
-})
+  schema: z.object({ title: z.string().optional() }),
+});
 
 export const collection = {
   pages: pageSchema,
+  news: newsSchema,
   articles: articleSchema,
   modelDescriptions: modelDescriptionSchema,
   modelAdditionalInfos: modelAdditionalInfoSchema,
   itemDescriptions: itemDescriptionSchema,
-}
+};
