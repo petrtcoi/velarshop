93cf2ff7c75449fdffdad2d7762141a62a09636a
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-07-10 13:15:38 +0300

    fix error

--- src/7-shared/utils/getModelSlug.ts
+++ src/7-shared/utils/getModelSlug.ts
@@ -1,6 +1,6 @@
 import { ModelJson } from '@entities/Model'
 
-export function getModelSlug(model: ModelJson): string {
+export function getModelSlug(model: Pick<ModelJson, 'type' | 'slug'>): string {
 	if (model.type === 'columns') return `/columns/${model.slug}`
 
 	return `/model/${model.slug}`
