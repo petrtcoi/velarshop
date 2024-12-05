7414c09861efeae6a9ca40bb8ba6a3523eac7313
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-12-05 19:24:40 +0300

    update robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,33 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import preact from '@astrojs/preact';
-import sitemap from '@astrojs/sitemap';
-import robotsTxt from 'astro-robots-txt';
+import image from '@astrojs/image'
+import mdx from '@astrojs/mdx'
+import preact from '@astrojs/preact'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://velarshop.ru',
-  integrations: [
-    tailwind(),
-    image(),
-    mdx(),
-    preact(),
-    sitemap(),
-    robotsTxt({
-      sitemap: ['https://velarshop.ru/sitemap-index.xml'],
-      policy: [
-        {
-          userAgent: '*',
-          allow: '/',
-          disallow: ['/model/*/*/'],
-        },
-      ],
-    }),
-  ],
-});
+	site: 'https://velarshop.ru',
+	integrations: [
+		tailwind(),
+		image(),
+		mdx(),
+		preact(),
+		sitemap(),
+		robotsTxt({
+			sitemap: ['https://velarshop.ru/sitemap-index.xml'],
+			policy: [
+				{
+					userAgent: '*',
+					allow: '/',
+					disallow: ['/model/*/*/'],
+				},
+				{
+					userAgent: 'Yandex',
+					'Clean-param': 'etext',
+				},
+			],
+		}),
+	],
+})
