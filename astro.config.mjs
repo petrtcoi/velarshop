6a6361266332126e425d568b2bbd23b07c7e3763
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2024-12-05 19:37:44 +0300

    update robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,10 @@ import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://velarshop.ru',
-	integrations: [
-		tailwind(),
-		image(),
-		mdx(),
-		preact(),
-		sitemap(),
-		robotsTxt({
-			sitemap: ['https://velarshop.ru/sitemap-index.xml'],
-			policy: [
-				{
-					userAgent: '*',
-					allow: '/',
-					disallow: ['/model/*/*/'],
-				},
-				{
-					userAgent: 'Yandex',
-					'Clean-param': 'etext',
-				},
-			],
-		}),
-	],
+	integrations: [tailwind(), image(), mdx(), preact(), sitemap()],
 })
