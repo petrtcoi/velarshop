8b97e6809e2d83d5e2e8abf478558398de298e0c
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-08-19 18:18:22 +0300

    add n_spacing

--- src/4-widgets/pageModel/RadiatorList/components/RadiatorRow.tsx
+++ src/4-widgets/pageModel/RadiatorList/components/RadiatorRow.tsx
@@ -1,71 +1,73 @@
-import { useStore } from "@nanostores/preact"
+import { useStore } from '@nanostores/preact'
 
-import BuyButton from "@features/order/ShoppingCart"
+import BuyButton from '@features/order/ShoppingCart'
 
-import { getGrillNotFit } from "@features/options/GrillNotFit"
-import { getRadiatorTotalCost } from "@features/radiator/RadiatorTotalCost"
+import { getGrillNotFit } from '@features/options/GrillNotFit'
+import { getRadiatorTotalCost } from '@features/radiator/RadiatorTotalCost'
 
-import type { ModelJson } from "@entities/Model"
-import type { RadiatorJson } from "@entities/Radiator"
-import { radiatorTotalTitle } from "@features/radiator/RadiatorTotalTitle"
+import type { ModelJson } from '@entities/Model'
+import type { RadiatorJson } from '@entities/Radiator'
+import { radiatorTotalTitle } from '@features/radiator/RadiatorTotalTitle'
 
 type Props = {
-  model: ModelJson
-  radiator: RadiatorJson
+	model: ModelJson
+	radiator: RadiatorJson
+	showInterAxis: boolean
 }
 
-function RadiatorRow({ model, radiator }: Props) {
-  const totalPrice = useStore(getRadiatorTotalCost)(model, radiator)
-  const totalTitle = useStore(radiatorTotalTitle)(model, radiator)
-  const convectorGrillNotFit = useStore(getGrillNotFit)(model, radiator)
+function RadiatorRow({ model, radiator, showInterAxis }: Props) {
+	const totalPrice = useStore(getRadiatorTotalCost)(model, radiator)
+	const totalTitle = useStore(radiatorTotalTitle)(model, radiator)
+	const convectorGrillNotFit = useStore(getGrillNotFit)(model, radiator)
 
-  return (
-    <tr class="hover:bg-neutral-50 py-3 text-xs border-b border-neutral-200 font-light">
-      <td class="py-3 pl-2 flex flex-col">
-        <div class="text-red-600 font-normal hover:underline">
-          <a href={`/model/${model.slug}/${radiator.slug}`}>{totalTitle}</a>
-        </div>
-        <div class="text-[10px] text-neutral-600 md:hidden">
-          {radiator.height}
-          <span class="text-[8px] font-thin">x</span>
-          {radiator.length}
-          <span class="text-[8px] font-thin">x</span>
-          {radiator.width} мм / {radiator.dt70} Вт
-        </div>
-      </td>
-      <td class="py-3 text-center hidden md:table-cell lg:hidden">
-        {radiator.height}
-        <span class="text-xs font-thin mx-1">x</span>
-        {radiator.length}
-        <span class="text-xs font-thin mx-1">x</span>
-        {radiator.width}
-      </td>
-      <td class="py-3 text-center hidden lg:table-cell">{radiator.width}</td>
-      <td class="py-3 text-center hidden lg:table-cell">{radiator.height}</td>
-      <td class="py-3 text-center hidden lg:table-cell">{radiator.length}</td>
-      <td class="py-3 text-center hidden md:table-cell">{radiator.dt70}</td>
-      {convectorGrillNotFit ? (
-        <td
-          class="py-3 text-neutral-400"
-          colSpan={2}
-        >
-          решетка не подходит
-        </td>
-      ) : (
-        <>
-          <td class="py-3">{totalPrice.toLocaleString("ru-RU")}</td>
-          <td class="justify-center ">
-            <BuyButton
-              itemTitle={totalTitle}
-              price={totalPrice}
-              model={model}
-              radiator={radiator}
-            />
-          </td>
-        </>
-      )}
-    </tr>
-  )
+	return (
+		<tr class='hover:bg-neutral-50 py-3 text-xs border-b border-neutral-200 font-light'>
+			<td class='py-3 pl-2 flex flex-col'>
+				<div class='text-red-600 font-normal hover:underline'>
+					<a href={`/model/${model.slug}/${radiator.slug}`}>{totalTitle}</a>
+				</div>
+				<div class='text-[10px] text-neutral-600 md:hidden'>
+					{radiator.height}
+					<span class='text-[8px] font-thin'>x</span>
+					{radiator.length}
+					<span class='text-[8px] font-thin'>x</span>
+					{radiator.width} мм / {radiator.dt70} Вт
+				</div>
+			</td>
+			<td class='py-3 text-center hidden md:table-cell lg:hidden'>
+				{radiator.height}
+				<span class='text-xs font-thin mx-1'>x</span>
+				{radiator.length}
+				<span class='text-xs font-thin mx-1'>x</span>
+				{radiator.width}
+			</td>
+			<td class='py-3 text-center hidden lg:table-cell'>{radiator.width}</td>
+			<td class='py-3 text-center hidden lg:table-cell'>{radiator.height}</td>
+			{showInterAxis ? <td class='py-3 text-center hidden lg:table-cell'>{radiator.n_spacing || ''}</td> : null}
+			<td class='py-3 text-center hidden lg:table-cell'>{radiator.length}</td>
+			<td class='py-3 text-center hidden md:table-cell'>{radiator.dt70}</td>
+			{convectorGrillNotFit ? (
+				<td
+					class='py-3 text-neutral-400'
+					colSpan={2}
+				>
+					решетка не подходит
+				</td>
+			) : (
+				<>
+					<td class='py-3'>{totalPrice.toLocaleString('ru-RU')}</td>
+					<td class='justify-center '>
+						<BuyButton
+							itemTitle={totalTitle}
+							price={totalPrice}
+							model={model}
+							radiator={radiator}
+						/>
+					</td>
+				</>
+			)}
+		</tr>
+	)
 }
 
 export default RadiatorRow
--- src/4-widgets/pageModel/RadiatorList/components/RadiatorsList.tsx
+++ src/4-widgets/pageModel/RadiatorList/components/RadiatorsList.tsx
@@ -1,221 +1,203 @@
-import { useEffect, useState } from "preact/hooks"
+import { useEffect, useState } from 'preact/hooks'
 
-import RadiatorRow from "./RadiatorRow"
-import RadiatorFilter from "./RadiatorFilter"
-import RadiatorListHeader from "./RadiatorListHeader"
+import RadiatorFilter from './RadiatorFilter'
+import RadiatorListHeader from './RadiatorListHeader'
+import RadiatorRow from './RadiatorRow'
 
-import SelectAddon from "@features/options/SelectAddons"
-import SelectConnection from "@features/options/SelectConnection"
-import SelectIroncastColor from "@features/options/SelectIroncastColor"
-import SelectConvectorGrill from "@features/options/SelectConvectorGrill"
+import SelectAddon from '@features/options/SelectAddons'
+import SelectConnection from '@features/options/SelectConnection'
+import SelectConvectorGrill from '@features/options/SelectConvectorGrill'
+import SelectIroncastColor from '@features/options/SelectIroncastColor'
 
-import { ALL } from "../constants/filterAll"
-import { filterRadiators } from "../utils/filterRadiators"
+import { ALL } from '../constants/filterAll'
+import { filterRadiators } from '../utils/filterRadiators'
 
-import type { ModelJson } from "@entities/Model"
-import type { RadiatorJson } from "@entities/Radiator"
-import SelectColumnConnection from "@features/options/SelectColumnConnection"
-import SelectColumnColor from "@features/options/SelectColumnColor"
+import type { ModelJson } from '@entities/Model'
+import type { RadiatorJson } from '@entities/Radiator'
+import SelectColumnColor from '@features/options/SelectColumnColor'
+import SelectColumnConnection from '@features/options/SelectColumnConnection'
 
 type Props = {
-  model: ModelJson
-  radiators: RadiatorJson[]
-  initialFilteredRadiators: RadiatorJson[]
-  heights: string[]
-  lengths: string[]
-  widths: string[]
-  filterByHeight?: boolean
-  filterByWidth?: boolean
-  filterByLength?: boolean
+	model: ModelJson
+	radiators: RadiatorJson[]
+	initialFilteredRadiators: RadiatorJson[]
+	heights: string[]
+	lengths: string[]
+	widths: string[]
+	filterByHeight?: boolean
+	filterByWidth?: boolean
+	filterByLength?: boolean
 }
 
 function RadiatorList(props: Props) {
-  const {
-    model,
-    radiators,
-    initialFilteredRadiators,
-    heights,
-    lengths,
-    widths,
-    filterByHeight = false,
-    filterByWidth = false,
-    filterByLength = false,
-  } = props
+	const {
+		model,
+		radiators,
+		initialFilteredRadiators,
+		heights,
+		lengths,
+		widths,
+		filterByHeight = false,
+		filterByWidth = false,
+		filterByLength = false,
+	} = props
 
-  const [lastFilterUpdate, setLastFilterUpdate] = useState<
-    "length" | "height" | "width"
-  >("height")
-  const [selectedHeight, setSelectedHeight] = useState<string>(
-    filterByHeight ? heights[0] : ALL
-  )
-  const [selectedWidth, setSelectedWidth] = useState<string>(
-    filterByWidth ? widths[0] : ALL
-  )
-  const [selectedLength, setSelectedLength] = useState<string>(
-    filterByLength ? lengths[0] : ALL
-  )
-  const [filteredRadiators, setFilteredRadiators] = useState<RadiatorJson[]>(
-    initialFilteredRadiators
-  )
+	const [lastFilterUpdate, setLastFilterUpdate] = useState<'length' | 'height' | 'width'>('height')
+	const [selectedHeight, setSelectedHeight] = useState<string>(filterByHeight ? heights[0] : ALL)
+	const [selectedWidth, setSelectedWidth] = useState<string>(filterByWidth ? widths[0] : ALL)
+	const [selectedLength, setSelectedLength] = useState<string>(filterByLength ? lengths[0] : ALL)
+	const [filteredRadiators, setFilteredRadiators] = useState<RadiatorJson[]>(initialFilteredRadiators)
 
-  // Track filter changes to avoid empty list
-  const changeHeightFilter = (height: string) => {
-    setSelectedHeight(height)
-    setLastFilterUpdate("height")
-  }
-  const changeWidthFilter = (width: string) => {
-    setSelectedWidth(width)
-    setLastFilterUpdate("width")
-  }
-  const changeLengthFilter = (length: string) => {
-    setSelectedLength(length)
-    setLastFilterUpdate("length")
-  }
+	const showInterAxis = model.type !== 'convector' && model.type !== 'floor'
 
-  useEffect(() => {
-    const radiator = radiators.find(
-      r =>
-        (selectedHeight === ALL || r.height === selectedHeight) &&
-        (selectedWidth === ALL || r.width === selectedWidth) &&
-        (selectedLength === ALL || r.length === selectedLength)
-    )
-    if (radiator) return
+	// Track filter changes to avoid empty list
+	const changeHeightFilter = (height: string) => {
+		setSelectedHeight(height)
+		setLastFilterUpdate('height')
+	}
+	const changeWidthFilter = (width: string) => {
+		setSelectedWidth(width)
+		setLastFilterUpdate('width')
+	}
+	const changeLengthFilter = (length: string) => {
+		setSelectedLength(length)
+		setLastFilterUpdate('length')
+	}
 
-    const escapeRadiator = radiators.find(
-      r =>
-        (selectedHeight === ALL ||
-          lastFilterUpdate !== "height" ||
-          r.height === selectedHeight) &&
-        (selectedWidth === ALL ||
-          lastFilterUpdate !== "width" ||
-          r.width === selectedWidth) &&
-        (selectedLength === ALL ||
-          lastFilterUpdate !== "length" ||
-          r.length === selectedLength)
-    )
-    // console.log(lastFilterUpdate, selectedHeight, selectedWidth)
-    // console.log("escapeRadiator", escapeRadiator)
-    if (escapeRadiator) {
-      if (lastFilterUpdate !== "height" && selectedHeight !== ALL)
-        setSelectedHeight(escapeRadiator.height)
-      if (lastFilterUpdate !== "width" && selectedWidth !== ALL)
-        setSelectedWidth(escapeRadiator.width)
-      if (lastFilterUpdate !== "length" && selectedLength !== ALL)
-        setSelectedLength(escapeRadiator.length)
-    }
-  }, [filteredRadiators])
+	useEffect(() => {
+		const radiator = radiators.find(
+			r =>
+				(selectedHeight === ALL || r.height === selectedHeight) &&
+				(selectedWidth === ALL || r.width === selectedWidth) &&
+				(selectedLength === ALL || r.length === selectedLength)
+		)
+		if (radiator) return
 
-  // Track filter changes to display not available options
-  const [availableHeights, setAvailableHeights] = useState<string[]>(heights)
-  const [availableWidths, setAvailableWidths] = useState<string[]>(widths)
-  const [availableLengths, setAvailableLengths] = useState<string[]>(lengths)
-  useEffect(() => {
-    setAvailableHeights(
-      heights.filter(h =>
-        radiators.some(
-          r =>
-            lastFilterUpdate === "height" ||
-            selectedHeight === ALL ||
-            (h === r.height &&
-              (selectedWidth === ALL || r.width === selectedWidth) &&
-              (selectedLength === ALL || r.length === selectedLength))
-        )
-      )
-    )
-    setAvailableWidths(
-      widths.filter(w =>
-        radiators.some(
-          r =>
-            lastFilterUpdate === "width" ||
-            selectedWidth === ALL ||
-            (w === r.width &&
-              (selectedHeight === ALL || r.height === selectedHeight) &&
-              (selectedLength === ALL || r.length === selectedLength))
-        )
-      )
-    )
-    setAvailableLengths(
-      lengths.filter(l =>
-        radiators.some(
-          r =>
-            lastFilterUpdate === "length" ||
-            selectedLength === ALL ||
-            (l === r.length &&
-              (selectedHeight === ALL || r.height === selectedHeight) &&
-              (selectedWidth === ALL || r.width === selectedWidth))
-        )
-      )
-    )
-  }, [filteredRadiators])
+		const escapeRadiator = radiators.find(
+			r =>
+				(selectedHeight === ALL || lastFilterUpdate !== 'height' || r.height === selectedHeight) &&
+				(selectedWidth === ALL || lastFilterUpdate !== 'width' || r.width === selectedWidth) &&
+				(selectedLength === ALL || lastFilterUpdate !== 'length' || r.length === selectedLength)
+		)
+		// console.log(lastFilterUpdate, selectedHeight, selectedWidth)
+		// console.log("escapeRadiator", escapeRadiator)
+		if (escapeRadiator) {
+			if (lastFilterUpdate !== 'height' && selectedHeight !== ALL) setSelectedHeight(escapeRadiator.height)
+			if (lastFilterUpdate !== 'width' && selectedWidth !== ALL) setSelectedWidth(escapeRadiator.width)
+			if (lastFilterUpdate !== 'length' && selectedLength !== ALL) setSelectedLength(escapeRadiator.length)
+		}
+	}, [filteredRadiators])
 
-  useEffect(() => {
-    setFilteredRadiators(
-      filterRadiators({
-        radiators,
-        selectedHeight,
-        selectedLength,
-        selectedWidth,
-      })
-    )
-  }, [selectedHeight, selectedWidth, selectedLength])
+	// Track filter changes to display not available options
+	const [availableHeights, setAvailableHeights] = useState<string[]>(heights)
+	const [availableWidths, setAvailableWidths] = useState<string[]>(widths)
+	const [availableLengths, setAvailableLengths] = useState<string[]>(lengths)
+	useEffect(() => {
+		setAvailableHeights(
+			heights.filter(h =>
+				radiators.some(
+					r =>
+						lastFilterUpdate === 'height' ||
+						selectedHeight === ALL ||
+						(h === r.height &&
+							(selectedWidth === ALL || r.width === selectedWidth) &&
+							(selectedLength === ALL || r.length === selectedLength))
+				)
+			)
+		)
+		setAvailableWidths(
+			widths.filter(w =>
+				radiators.some(
+					r =>
+						lastFilterUpdate === 'width' ||
+						selectedWidth === ALL ||
+						(w === r.width &&
+							(selectedHeight === ALL || r.height === selectedHeight) &&
+							(selectedLength === ALL || r.length === selectedLength))
+				)
+			)
+		)
+		setAvailableLengths(
+			lengths.filter(l =>
+				radiators.some(
+					r =>
+						lastFilterUpdate === 'length' ||
+						selectedLength === ALL ||
+						(l === r.length &&
+							(selectedHeight === ALL || r.height === selectedHeight) &&
+							(selectedWidth === ALL || r.width === selectedWidth))
+				)
+			)
+		)
+	}, [filteredRadiators])
 
-  return (
-    <div
-      id="radiators-list"
-      class="mt-7 pt-3"
-    >
-      {filterByLength && (
-        <RadiatorFilter
-          title={"Длина (мм)"}
-          options={lengths}
-          availableOptions={availableLengths}
-          selectedOption={selectedLength}
-          setSelectedOption={changeLengthFilter}
-        />
-      )}
-      {filterByHeight && (
-        <RadiatorFilter
-          title={"Высота (мм)"}
-          options={heights}
-          availableOptions={availableHeights}
-          selectedOption={selectedHeight}
-          setSelectedOption={changeHeightFilter}
-        />
-      )}
-      {filterByWidth && (
-        <RadiatorFilter
-          title={"Глубина (мм)"}
-          options={widths}
-          availableOptions={availableWidths}
-          selectedOption={selectedWidth}
-          setSelectedOption={changeWidthFilter}
-        />
-      )}
+	useEffect(() => {
+		setFilteredRadiators(
+			filterRadiators({
+				radiators,
+				selectedHeight,
+				selectedLength,
+				selectedWidth,
+			})
+		)
+	}, [selectedHeight, selectedWidth, selectedLength])
 
-      {(model.type === "design" || model.type === "floor") && (
-        <SelectConnection model={model} />
-      )}
-      {model.type === "columns" && <SelectColumnColor />}
-      {model.type === "columns" && <SelectColumnConnection model={model} />}
-      {model.type === "ironcast" && <SelectIroncastColor />}
-      {model.type === "convector" && <SelectConvectorGrill />}
-      <SelectAddon model={model} />
+	return (
+		<div
+			id='radiators-list'
+			class='mt-7 pt-3'
+		>
+			{filterByLength && (
+				<RadiatorFilter
+					title={'Длина (мм)'}
+					options={lengths}
+					availableOptions={availableLengths}
+					selectedOption={selectedLength}
+					setSelectedOption={changeLengthFilter}
+				/>
+			)}
+			{filterByHeight && (
+				<RadiatorFilter
+					title={'Высота (мм)'}
+					options={heights}
+					availableOptions={availableHeights}
+					selectedOption={selectedHeight}
+					setSelectedOption={changeHeightFilter}
+				/>
+			)}
+			{filterByWidth && (
+				<RadiatorFilter
+					title={'Глубина (мм)'}
+					options={widths}
+					availableOptions={availableWidths}
+					selectedOption={selectedWidth}
+					setSelectedOption={changeWidthFilter}
+				/>
+			)}
 
-      <div class="mt-5">
-        <table class="text-xs w-full text-left">
-          <RadiatorListHeader />
-          <tbody>
-            {filteredRadiators.map(radiator => (
-              <RadiatorRow
-                model={model}
-                radiator={radiator}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  )
+			{(model.type === 'design' || model.type === 'floor') && <SelectConnection model={model} />}
+			{model.type === 'columns' && <SelectColumnColor />}
+			{model.type === 'columns' && <SelectColumnConnection model={model} />}
+			{model.type === 'ironcast' && <SelectIroncastColor />}
+			{model.type === 'convector' && <SelectConvectorGrill />}
+			<SelectAddon model={model} />
+
+			<div class='mt-5'>
+				<table class='text-xs w-full text-left'>
+					<RadiatorListHeader showInterAxis={showInterAxis} />
+					<tbody>
+						{filteredRadiators.map(radiator => (
+							<RadiatorRow
+								model={model}
+								radiator={radiator}
+								showInterAxis={showInterAxis}
+							/>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
 }
 
 export default RadiatorList
