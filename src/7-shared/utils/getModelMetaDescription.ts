6d6266b8e0a185164674126b5975e89d40523cc8
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-09-20 10:37:06 +0200

    fix canonicals and descriptions

--- src/7-shared/utils/getModelMetaDescription.ts
+++ src/7-shared/utils/getModelMetaDescription.ts
@@ -1,23 +1,18 @@
-import type { ModelJson } from "@entities/Model"
+import type { ModelJson } from '@entities/Model'
 
 export const getModelMetaDescription = (model: ModelJson): string => {
+	const { name, prefix, orientation } = model
+	//TODO add description meta for model
 
-  const { name, prefix, orientation } = model
-  //TODO add description meta for model
+	let title = ''
+	title += `${prefix} Velar ${name} `
 
-  let title = ''
-  title += `${ prefix } Velar ${ name } `
+	if (orientation) {
+		title +=
+			orientation === 'horizontal' ? 'горизонтальная модель' : orientation === 'vertical' ? 'вертикальная модель' : ''
+	}
 
-  if (orientation) {
-    title += orientation === 'horizontal'
-      ? 'горизонтальная модель'
-      : orientation === 'vertical'
-        ? 'вертикальная модель'
-        : ''
-  }
+	const description = `Купить ${prefix} Velar ${name} с доставкой по России. Официальная гарантия. Полный модельный ряд и быстрые сроки производства.`
 
-
-
-
-  return title.trim()
+	return description
 }
