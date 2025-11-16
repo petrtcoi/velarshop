35ad0fb32a95da67403df1a2cc1485b73ae653d6
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-11-16 21:43:16 +0100

    fix

--- src/7-shared/components/GoToFormButton.tsx
+++ src/7-shared/components/GoToFormButton.tsx
@@ -1,6 +1,6 @@
-import { storeCartTotalQnty } from '@features/order/ShoppingCart'
-import { useStore } from '@nanostores/preact'
-import { useEffect, useState } from 'preact/hooks'
+import { storeCartTotalQnty } from '@features/order/ShoppingCart';
+import { useStore } from '@nanostores/preact';
+import { useEffect, useState } from 'preact/hooks';
 
 export function GoToFormButton() {
 	const [isOpen, setIsOpen] = useState(false)
@@ -24,7 +24,7 @@ export function GoToFormButton() {
 			class='hover:opacity-90 z-100'
 		>
 			<div class='bg-teal-600 z-100 fixed bottom-0 left-0 right-0  md:w-[320px] flex flex-col gap-[2px] h-[50px] items-center justify-center md:rounded-tr-xl text-white'>
-				<div class='text-[12px] leading-[13px] font-light'>🔥 Получить расчет и финальную цену в течение дня</div>
+				<div class='text-[12px] leading-[13px] font-light'>🔥 Получить расчет и цену в течение дня</div>
 				<div class='text-[10px] leading-[10px font-light'>Пришлем предложение на почту или в мессенджер</div>
 			</div>
 		</a>
