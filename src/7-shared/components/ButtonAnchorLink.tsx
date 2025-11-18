a3c89cee7058ee331416d2252d5b0f9cf60880c5
petrtcoi <45001736+petrtcoi@users.noreply.github.com>
2025-11-18 10:50:58 +0100

    update UI

--- src/7-shared/components/ButtonAnchorLink.tsx
+++ src/7-shared/components/ButtonAnchorLink.tsx
@@ -1,28 +1,23 @@
 type Props = {
-  title: string
-  anchor: string
+	title: string
+	anchor: string
 }
 
-function ButtonAnchorLink ({ title, anchor }: Props) {
+function ButtonAnchorLink({ title, anchor }: Props) {
+	const hrefLink = location.protocol + '//' + location.host + location.pathname + anchor
 
-  const hrefLink = location.protocol + '//' + location.host + location.pathname + anchor
-
-
-  return (
-    <button
-      aria-label="Перейти к списку радиаторов модели"
-      class='bg-transparent border-red-600 border hover:bg-red-600 py-1 px-4 rounded-lg group transition-colors'
-      onClick={ () => {
-        if (!window?.location) return
-        window.location.href = hrefLink
-      } }
-    >
-      <span class="text-red-600 text-xs group-hover:text-white transition-colors">{ title }</span>
-    </button>
-  )
+	return (
+		<button
+			aria-label='Перейти к списку радиаторов модели'
+			class='bg-transparent border-neutral-700 border hover:bg-neutral-900 py-1 px-4 rounded-lg group transition-colors'
+			onClick={() => {
+				if (!window?.location) return
+				window.location.href = hrefLink
+			}}
+		>
+			<span class='text-neutral-700 text-xs relative -top-[1px] group-hover:text-white transition-colors'>{title}</span>
+		</button>
+	)
 }
 
 export default ButtonAnchorLink
-
-
-
